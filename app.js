f5a269eece3b7978793af89b8de003c3b13bb6b6
Gannon Prudhomme <gannonprudhomme@gmail.com>
2019-02-17 01:59:54 -0600

    Imported socket.js in app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express'),
       path = require('path'),
       http = require('http');
-
+    
 const app = express()
 const port = 3001
 
 var server = http.createServer(app)
 var io = require('socket.io').listen(server)
+
+var socket = require('./routes/sockets.js')(io)
+
+app.use(socket)
 
 server.listen(port, (err) => {
     if(err) {
